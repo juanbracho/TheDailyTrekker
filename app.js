5de3045ec4d0d3b2179bbd3d46b0c163b6218d22
Juan Bracho <64031972+juanbracho@users.noreply.github.com>
2024-02-19 14:02:57 -0600

    Update app.js

--- app.js
+++ app.js
@@ -47,7 +47,7 @@ PageTransitions();
 //Appointment button email template
 document.getElementById("emailButton").addEventListener("click", function() {
     var subject = "Appointment Request with Eric Miller";
-    var body = "I would like to request an appointment on (Date) at (Time) with Eric Miller under Dr. Walters.";
+    var body = "I would like to request an appointment on (Date) at (Time) with Eric Miller under Dr. Walters. Thanks!";
   
     var mailtoLink = "mailto:askparkerclinics@parker.edu" +
                      "?subject=" + encodeURIComponent(subject) +
