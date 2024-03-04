3da009fe2b125e8c21d7621243e6915f321539c1
Juan Bracho <64031972+juanbracho@users.noreply.github.com>
2024-03-04 11:46:27 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,65 @@
+document.getElementById("navButton").addEventListener("click", function() {
+    var subject = "Appointment Request with Eric Miller Under Dr. Walters";
+    var body = "I would like to request an appointment on (Date) at (Time) with Eric Miller under Dr. Walters. Thanks!";
+  
+    var mailtoLink = "mailto:askparkerclinics@parker.edu" +
+                     "?subject=" + encodeURIComponent(subject) +
+                     "&body=" + encodeURIComponent(body);
+  
+    window.location.href = mailtoLink;
+  });
+
+  document.getElementById("heroButton").addEventListener("click", function() {
+    var subject = "Appointment Request with Eric Miller Under Dr. Walters";
+    var body = "I would like to request an appointment on (Date) at (Time) with Eric Miller under Dr. Walters. Thanks!";
+  
+    var mailtoLink = "mailto:askparkerclinics@parker.edu" +
+                     "?subject=" + encodeURIComponent(subject) +
+                     "&body=" + encodeURIComponent(body);
+  
+    window.location.href = mailtoLink;
+  });
+
+  document.getElementById("service1").addEventListener("click", function() {
+    var subject = "Appointment Request with Eric Miller Under Dr. Walters";
+    var body = "I would like to request an appointment on (Date) at (Time) with Eric Miller under Dr. Walters. Thanks!";
+  
+    var mailtoLink = "mailto:askparkerclinics@parker.edu" +
+                     "?subject=" + encodeURIComponent(subject) +
+                     "&body=" + encodeURIComponent(body);
+  
+    window.location.href = mailtoLink;
+  });
+
+  document.getElementById("service2").addEventListener("click", function() {
+    var subject = "Appointment Request with Eric Miller Under Dr. Walters";
+    var body = "I would like to request an appointment on (Date) at (Time) with Eric Miller under Dr. Walters. Thanks!";
+  
+    var mailtoLink = "mailto:askparkerclinics@parker.edu" +
+                     "?subject=" + encodeURIComponent(subject) +
+                     "&body=" + encodeURIComponent(body);
+  
+    window.location.href = mailtoLink;
+  });
+
+  document.getElementById("service3").addEventListener("click", function() {
+    var subject = "Appointment Request with Eric Miller Under Dr. Walters";
+    var body = "I would like to request an appointment on (Date) at (Time) with Eric Miller under Dr. Walters. Thanks!";
+  
+    var mailtoLink = "mailto:askparkerclinics@parker.edu" +
+                     "?subject=" + encodeURIComponent(subject) +
+                     "&body=" + encodeURIComponent(body);
+  
+    window.location.href = mailtoLink;
+  });
+
+  document.getElementById("4").addEventListener("click", function() {
+    var subject = "Appointment Request with Eric Miller Under Dr. Walters";
+    var body = "I would like to request an appointment on (Date) at (Time) with Eric Miller under Dr. Walters. Thanks!";
+  
+    var mailtoLink = "mailto:askparkerclinics@parker.edu" +
+                     "?subject=" + encodeURIComponent(subject) +
+                     "&body=" + encodeURIComponent(body);
+  
+    window.location.href = mailtoLink;
+  });
